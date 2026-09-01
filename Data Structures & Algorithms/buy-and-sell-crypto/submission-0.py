1e21b91c0c255b4559a39a47fe17034ae0464d36
Chris Cortes <christophercortes@ucsb.edu>
2026-08-31 20:36:14 -0700

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -3,16 +3,17 @@ class Solution:
         n = len(prices)
         max_profit = 0
         l = 0
-        r = l + 1
+        r = 1
 
         while r < n:
             curr_profit = prices[r] - prices[l]
             max_profit = max(max_profit, curr_profit)
 
-            if prices[l] > prices[r]:
+            if curr_profit < 0:
                 l = r
                 r += 1
             else:
                 r += 1
-        
+
         return max_profit
+                 
